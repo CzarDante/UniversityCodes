bda26513a43e973e6592762a89e0e730c039188b
CzarDante <duducezarfernandes@gmail.com>
2020-12-10 14:40:16 -0300

    10122020

--- Apc_2/prova20202/q2.c
+++ Apc_2/prova20202/q2.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define lin 30
+#include <string.h>
+#define linhas 30
 
-const int TAM = 52;
-int Pesq_Str ( char **str , char busca [ 30 ] ) {
-    int i, j, k;
+int i, j, k;
+const int caracters = 30;
+
+int Pesq_Str ( char **str , char busca[30]){
     printf ( "Digite uma string para pesquisar : " );
-    scanf ( "%30[^\n]s" , busca );
-    fflush ( stdin );
-    for ( i = 0; i < lin ; i++ ) {
-        for ( j = 0; j < TAM ; j++ ) {
+    fflush(stdin);
+    fgets(busca,30,stdin);
+    busca[strlen(busca)-1] = '\0';
+    for(i=0;i<linhas;i++){
+        for (j=0;j<caracters;j++ ){
             k = 0;
             while ( str [ i ] [ j + k ] == busca [ k ] && busca [ k ] != '\0' && str [ i ] [ j + k ] != '\0' ) {
                 k++;
                 if ( busca [ k ] == '\0' ) {
-                    printf ( "A string %s foi achada",busca);
+                    printf ( "A string %s foi achada\n",busca);
                 }
+            }
+            if(i == (linhas-1) && j == (caracters-1)){
+                printf("A string nao foi achada ");
             }
         }
     }
@@ -30,11 +36,11 @@ int main ( ) {
     printf("nome do arquivo: \n");
     gets(nomearquivo);
     arq = fopen(nomearquivo, "r");
-    str = ( char** ) malloc ( TAM * sizeof(char*) );
+    str = ( char** ) malloc ( caracters * sizeof(char*) );
     if ( arq != NULL ) {
         while ( feof ( arq ) == 0 ) {
-            str [ i ] = ( char* ) malloc ( TAM * sizeof(char) );
-            fgets ( str [ i ] , TAM , arq );
+            str [ i ] = ( char* ) malloc ( caracters * sizeof(char) );
+            fgets ( str [ i ] , caracters , arq );
             i++;
         }
         n = Pesq_Str (str , busca );    
---
+++ python/fizzbuzz.py
@@ -0,0 +1,18 @@
+import time
+import colorama
+from colorama import Fore,Back,Style,init
+init()
+red = "\03[1;31m"
+end = "\033[0;0m"
+for i in range(100000):
+    num = i
+    #time.sleep(1)
+    if num%3==0:
+        if num%5==0:
+            print(Fore.RED + "fizz buzz")
+        else:
+            print(Fore.YELLOW +"fizz")
+    elif num%5==0:
+            print(Fore.GREEN + "buzz")
+    else:
+        print(Fore.WHITE + f"{num} ")
