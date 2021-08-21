8796819ecd778297de932dd1d6982dba951df81f
CzarDante <duducezarfernandes@gmail.com>
2021-08-21 08:21:16 -0300

    uri

--- Uri/1024.c
+++ Uri/1024.c
@@ -1,28 +1,29 @@
-#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 int main()
 {
     char str[1000], str1[1000];
-    int qtd_strings,tamanho_string,inverte;
+    int qtd_strings,tamanho_string,inverte,i;
     scanf("%d",&qtd_strings);
     getchar();
     while (qtd_strings--)
     {
         gets(str);
         tamanho_string=strlen(str);
-        for (int i = 0; i < tamanho_string; i++)
+        for(i = 0; i < tamanho_string; i++)
         {
             if(str[i]>='A' && str[i]<='Z' || str[i]>='a' && str[i]<='z')
                 str[i] = str[i] + 3;
         }
         inverte=0;
-        for(int i=tamanho_string-1;i>=0;i--)
+        for(i=tamanho_string-1;i>=0;i--)
         {
             str1[inverte] = str[i];
             inverte++;
         }
         str1[inverte] = '\0';
-        for(int i=tamanho_string/2;i<tamanho_string;i++)
+        for(i=tamanho_string/2;i<tamanho_string;i++)
         {
             str1[i] =  str1[i] - 1;
         }
